70367caa323c5206c197b7dc2ec43f182ac08f4f
Austin Riendeau <austin@dayforward.com>
2022-02-17 16:27:35 -0700

    strip defaults

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="header">
+    <div class="title">
+      <h1>riptide rhythm</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+  @import "../assets/variables.less";
+  .header {
+    height: 250px;
+    background-color: #2a2a2a;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    & .title{
+      margin-top: auto;
+      align-self: flex-start;
+      width: auto;
+      padding: 0px 10px;
+      & h1 {
+        color: @white;
+        font-size: 2.1em;
+        padding: 5px;
+        margin: 0 0 -25px 0;
+      }
+    }
+  }
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,19 @@
+<script setup>
+import Header from '@/components/Header.vue'
+</script>
+
 <template>
+  <Header/>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an my page</h1>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    min-height: auto;
     display: flex;
-    align-items: center;
   }
 }
 </style>
